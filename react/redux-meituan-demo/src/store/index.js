5b7c7e4e43dc0cdf5a8a2bf1c7b11cd6cd30387b
Miloelsie <2607439502@qq.com>
2023-12-27 23:45:46 +0800

    redux-meituan-demo

---
+++ react/redux-meituan-demo/src/store/index.js
@@ -0,0 +1,10 @@
+import foodsReducer from './module/foodSlice'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {
+    foods: foodsReducer
+  }
+})
+
+export default store
